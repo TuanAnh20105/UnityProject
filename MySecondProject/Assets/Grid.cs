e7a2c6e6ecfed506e1e304ebec5ed91d83818f9c
TuanAnh20105 <anhntgch190543@fpt.edu.vn>
2022-05-24 17:35:01 +0700

    ok

---
+++ MySecondProject/Assets/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 girdWorldSize;
+    public float nodeRadius;
+    public LayerMask unwalkableMask;
+    Node[,] grid;
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector2(girdWorldSize.x, girdWorldSize.y));
+    }
+    private void Start() {
+        
+    }
+}
